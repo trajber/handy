169cfe8ba1f0c1ec488e2d878042a6d714e216a9
Mauro Romano Trajber <mauro@registro.br>
2014-05-28 15:49:24 -0300

    Using a new way to chain interceptors

--- interceptor.go
+++ interceptor.go
@@ -2,7 +2,11 @@ package handy
 
 import "net/http"
 
-type Interceptor func(w http.ResponseWriter, r *http.Request, h Handler)
+type Interceptor interface {
+	Before(w http.ResponseWriter, r *http.Request, h Handler) error
+	After(w http.ResponseWriter, r *http.Request, h Handler)
+	OnError(w http.ResponseWriter, r *http.Request, h Handler, err error)
+}
 
 type InterceptorChain []Interceptor
 
@@ -17,10 +21,31 @@ func NewInterceptorChain() InterceptorChain {
 
 type NopInterceptorChain struct{}
 
-func (n *NopInterceptorChain) Before() InterceptorChain {
+func (n *NopInterceptorChain) Interceptors() InterceptorChain {
 	return NewInterceptorChain()
 }
 
-func (n *NopInterceptorChain) After() InterceptorChain {
-	return NewInterceptorChain()
+type BeforeInterceptorFunc func(w http.ResponseWriter, r *http.Request, h Handler) error
+
+func (i BeforeInterceptorFunc) Before(w http.ResponseWriter, r *http.Request, h Handler) error {
+	return i(w, r, h)
 }
+
+func (i BeforeInterceptorFunc) After(w http.ResponseWriter, r *http.Request, h Handler)              {}
+func (i BeforeInterceptorFunc) OnError(w http.ResponseWriter, r *http.Request, h Handler, err error) {}
+
+type AfterInterceptorFunc func(w http.ResponseWriter, r *http.Request, h Handler)
+
+func (i AfterInterceptorFunc) Before(w http.ResponseWriter, r *http.Request, h Handler) error {
+	return nil
+}
+
+func (i AfterInterceptorFunc) After(w http.ResponseWriter, r *http.Request, h Handler) {
+	i(w, r, h)
+}
+
+func (i AfterInterceptorFunc) OnError(w http.ResponseWriter, r *http.Request, h Handler, err error) {}
+
+type NoErrorInterceptor struct{}
+
+func (i NoErrorInterceptor) OnError(w http.ResponseWriter, r *http.Request, h Handler, err error) {}
