afa4ea10e804ff235e3ac42111efe3dd91d1746e
Gustavo Henrique Montesião de Sousa <gustavo@registro.br>
2015-04-10 16:23:33 -0300

    Move InterceptorFuncs to interceptor package

--- interceptor/basic.go
+++ interceptor/basic.go
@@ -16,3 +16,23 @@ type NopInterceptor struct{}
 
 func (i *NopInterceptor) Before(w http.ResponseWriter, r *http.Request) {}
 func (i *NopInterceptor) After(w http.ResponseWriter, r *http.Request)  {}
+
+type BeforeInterceptorFunc func() int
+
+func (i BeforeInterceptorFunc) Before() int {
+	return i()
+}
+
+func (i BeforeInterceptorFunc) After(int) int {
+	return 0
+}
+
+type AfterInterceptorFunc func(int) int
+
+func (i AfterInterceptorFunc) Before() int {
+	return 0
+}
+
+func (i AfterInterceptorFunc) After(status int) int {
+	return i(status)
+}
