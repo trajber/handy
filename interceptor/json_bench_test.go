ddf172e56a0efc3057575b8a12b2562b6fe6ecd5
Gustavo Henrique Montesião de Sousa <gustavo@registro.br>
2015-04-10 16:27:37 -0300

    Adapt the JSON and RequestLogger interceptors to the new interface

--- interceptor/json_bench_test.go
+++ interceptor/json_bench_test.go
@@ -16,7 +16,7 @@ type TestStruct struct {
 var (
 	payload = `
 {
-	"name":"foo", 
+	"name":"foo",
 	"id":10
 }`
 )
@@ -30,15 +30,17 @@ func BenchmarkDecodeJSON(b *testing.B) {
 	w := httptest.NewRecorder()
 
 	handler := new(struct {
+		IntrospectorEmbedded
 		handy.DefaultHandler
 		Request TestStruct `request:"get"`
 	})
+	handler.DefaultHandler = handy.BuildDefaultHandler(w, req, nil)
 
 	codec := NewJSONCodec(handler)
 
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		codec.Before(w, req)
+		codec.Before()
 		if handler.Request.Id != 10 {
 			b.Fail()
 		}
