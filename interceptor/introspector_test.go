a95e6a2789f0ba01223cc9d69ab3dd0c78bddc68
Gustavo Henrique Montesião de Sousa <gustavo@registro.br>
2020-09-30 09:29:25 -0300

    Simplify Introspector tests
    With the changes in the way the interceptors work, there's no need for a
    handler embeds an introspector anymore.

--- interceptor/introspector_test.go
+++ interceptor/introspector_test.go
@@ -1,28 +1,21 @@
 package interceptor_test
 
 import (
-	"github.com/kylelemons/godebug/pretty"
 	"handy"
 	"handy/interceptor"
 	"testing"
+
+	"github.com/kylelemons/godebug/pretty"
 )
 
 func TestIntrospector(t *testing.T) {
 	type introspectable struct {
-		handy.ProtoHandler
-		interceptor.IntrospectorAPI
+		handy.BaseHandler
 
 		First  int    `one:"um"`
 		Second string `two:"dos,dois"`
 		Third  int
 		Fourth *bool `other:"unrelated" four:"cinco,quatro"`
-	}
-
-	newIntrospectable := func(object *introspectable) (handy.Handler, handy.Interceptor) {
-		intro := interceptor.NewIntrospector(nil, object)
-		object.IntrospectorAPI = intro
-
-		return object, intro
 	}
 
 	isTrue := true
@@ -33,89 +26,83 @@ func TestIntrospector(t *testing.T) {
 		Fourth: &isTrue,
 	}
 
-	newIntrospectable(object)
+	intro := interceptor.NewIntrospector(nil, object)
 
-	if first, ok := object.Field("one", "um").(*int); !ok || &object.First != first {
-		t.Errorf("It didn't retrieved the right value. Expecting “%#v”; found “%#v”", first, object.Field("one", "um"))
+	if first, ok := intro.Field("one", "um").(*int); !ok || &object.First != first {
+		t.Errorf("It didn't retrieved the right value. Expecting “%#v”; found “%#v”", first, intro.Field("one", "um"))
 
-	} else if values := object.KeysWithTag("one"); len(values) != 1 {
+	} else if values := intro.KeysWithTag("one"); len(values) != 1 {
 		t.Errorf("Wrong number of values for tag “one”: “%d”", len(values))
 	}
 
-	if second, ok := object.Field("two", "dois").(*string); !ok || &object.Second != second {
-		t.Errorf("It didn't retrieved the right value. Expecting “%#v”; found “%#v”", second, object.Field("second", "dois"))
+	if second, ok := intro.Field("two", "dois").(*string); !ok || &object.Second != second {
+		t.Errorf("It didn't retrieved the right value. Expecting “%#v”; found “%#v”", second, intro.Field("second", "dois"))
 
-	} else if values := object.KeysWithTag("two"); len(values) != 2 {
+	} else if values := intro.KeysWithTag("two"); len(values) != 2 {
 		t.Errorf("Wrong number of values for tag “two”: “%d”", len(values))
 	}
 
-	if fourth, ok := object.Field("four", "quatro").(*bool); !ok || object.Fourth != fourth {
-		t.Errorf("It didn't retrieved the right value. Expecting “%#v”; found “%#v”", fourth, object.Field("fourth", "quatro"))
+	if fourth, ok := intro.Field("four", "quatro").(*bool); !ok || object.Fourth != fourth {
+		t.Errorf("It didn't retrieved the right value. Expecting “%#v”; found “%#v”", fourth, intro.Field("fourth", "quatro"))
 
-	} else if values := object.KeysWithTag("four"); len(values) != 2 {
+	} else if values := intro.KeysWithTag("four"); len(values) != 2 {
 		t.Errorf("Wrong number of values for tag “four”: “%d”", len(values))
 	}
 
 	newFourth := false
 	object.Fourth = &newFourth
 
-	if fourth, ok := object.Field("four", "quatro").(*bool); !ok || object.Fourth != fourth {
-		t.Errorf("It didn't retrieved the right value. Expecting “%#v”; found “%#v”", fourth, object.Field("fourth", "quatro"))
+	if fourth, ok := intro.Field("four", "quatro").(*bool); !ok || object.Fourth != fourth {
+		t.Errorf("It didn't retrieved the right value. Expecting “%#v”; found “%#v”", fourth, intro.Field("fourth", "quatro"))
 	}
 
 	// See if the check for a nil value works as expected
+	object = new(introspectable)
+	intro = interceptor.NewIntrospector(nil, object)
 
-	object = &introspectable{}
-	newIntrospectable(object)
-
-	if field := object.Field("four", "quatro"); field != nil {
+	if field := intro.Field("four", "quatro"); field != nil {
 		t.Errorf("This value is supposed to be nil: %#v", field)
 	}
 
 	object.Fourth = &isTrue
 
-	if field := object.Field("four", "quatro"); field == nil {
+	if field := intro.Field("four", "quatro"); field == nil {
 		t.Errorf("This value isn't supposed to be nil: %#v", field)
 	}
 }
 
 func TestIntrospectorCanNotInterface(t *testing.T) {
 	object := struct {
-		interceptor.IntrospectorAPI
 		f int `field:"f"`
 	}{}
 	intro := interceptor.NewIntrospector(nil, &object)
-	object.IntrospectorAPI = intro
 
-	if f := object.Field("field", "f"); f != nil {
+	if f := intro.Field("field", "f"); f != nil {
 		t.Errorf("The value %#v is supposed to be nil", f)
 	}
 }
 
 func TestIntrospectorUnknownField(t *testing.T) {
 	object := struct {
-		interceptor.IntrospectorAPI
 		F int `field:"f"`
 	}{}
 	intro := interceptor.NewIntrospector(nil, &object)
-	object.IntrospectorAPI = intro
 
 	// It shouldn't change the state of the object
-
 	copied := object
-	object.SetField("missing", "field", 17)
+	intro.SetField("missing", "field", 17)
 
 	if copied.F != object.F {
 		t.Errorf("Both objects are expected to be equal:\n%s", pretty.Compare(copied, object))
 	}
 
-	object.SetField("field", "g", 17)
+	intro.SetField("field", "g", 17)
 
 	if copied.F != object.F {
 		t.Errorf("Both objects are expected to be equal:\n%s", pretty.Compare(copied, object))
 	}
 
-	f := object.Field("missing", "field")
+	f := intro.Field("missing", "field")
 
 	if f != nil {
 		t.Errorf("The value %#v is supposed to be nil", f)
@@ -124,16 +111,14 @@ func TestIntrospectorUnknownField(t *testing.T) {
 
 func TestIntrospectorEmbedded(t *testing.T) {
 	object := struct {
-		interceptor.IntrospectorAPI
 		dummy
 	}{}
 	intro := interceptor.NewIntrospector(nil, &object)
-	object.IntrospectorAPI = intro
 
-	if _, ok := object.Field("field", "f").(*int); !ok {
+	if _, ok := intro.Field("field", "f").(*int); !ok {
 		t.Error("It didn't identify the object")
 
-	} else if values := object.KeysWithTag("field"); len(values) != 1 {
+	} else if values := intro.KeysWithTag("field"); len(values) != 1 {
 		t.Errorf("Wrong number of values for tag “field”: “%d”", len(values))
 	}
 }
